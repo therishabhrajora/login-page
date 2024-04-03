dbef9c24175053ce801976cb12194d05571cd7ba
therishabhrajora <rishabhrajora20@gmail.com>
2024-04-03 23:06:12 +0530

    modified resetpassword

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ document.querySelector(".register-btn").addEventListener("click", registerOn = (
 document.querySelector(".login-btn").addEventListener("click", loginOn2 = () => {
     document.querySelector(".login").style.display = "block";
     document.querySelector(".register").style.display = "none";
-    
+
 
 });
 
@@ -42,19 +42,23 @@ document.querySelector(".close-resetbtn").addEventListener("click", closeresetwi
 })
 
 
-const users = [];
+
 function loginfunc() {
     loginusername = document.querySelector(".input_loginusername").value;
     loginpassword = document.querySelector(".input_loginpassword").value;
+    document.querySelector(".submit-login").addEventListener("click", () => {
+        if (loginusername === registerusername  && loginpassword === registerpassword) {
+            alert("you are logined")
+            console.log("you are login",users)
 
-    if (loginusername === registerusername && loginpassword === registerpassword) {
-        alert("you are logined")
-    } else if (loginusername != registerusername) {
-        document.querySelector(".wrong-username").innerHTML = `username not match*`
-    } else {
-        document.querySelector(".wrong-password").innerHTML = `password not match*`
-    }
-}
+        } else if (loginusername != registerusername) {
+            document.querySelector(".wrong-username").innerHTML = `username not match*`
+        } else {
+            document.querySelector(".wrong-password").innerHTML = `password not match*`
+        }
+    })
+    };
+
 
 function resetpasswordfunc() {
     currentpassword = document.querySelector(".input_currentpassword").value;
@@ -64,36 +68,30 @@ function resetpasswordfunc() {
     reenterpassword = document.querySelector(".input_reenterpassword").value;
     console.log(reenterpassword);
     if (currentpassword === registerpassword && newpassword === reenterpassword) {
-        registerpassword = newpassword;
+        console.log("done")
+        registerpassword=newpassword;
+        users[0].password=newpassword;
         alert("password changed");
+        console.log("password changed",users);
     } else if (currentpassword != registerpassword) {
         document.querySelector(".notcurrentpsw").innerHTML = `Enter your Current password.*`
     } else {
         document.querySelector(".pswnotmatch").innerHTML = `please re-enter password.*`
     }
-    
-
 }
+const users = [];
 const register = document.querySelector(".submit-register");
-
 function adduser() {
-
     registerusername = document.querySelector(".input_registerusername").value;
     registeremail = document.querySelector(".input_registeremail").value;
     registerpassword = document.querySelector(".input_registerpassword").value;
-
     users.push({ username: registerusername, email: registeremail, password: registerpassword });
     alert("you are registered");
+    console.log(users)
     document.querySelector(".register").style.display = "none";
     document.querySelector(".login").style.display = "block";
-
-    const login = document.querySelector(".submit-login");
-    login.addEventListener("click", loginfunc);
-
-    const reset = document.querySelector(".submit-continue");
-    reset.addEventListener("click", resetpasswordfunc);
-
-
+    document.querySelector(".submit-login").addEventListener("click", loginfunc);
+    document.querySelector(".submit-continue").addEventListener("click", resetpasswordfunc);
 }
 register.addEventListener("click", adduser);
 
